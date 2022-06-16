2cc4a9acc87c10f099dd88f8b01d8eb06445ba86
pzyxo <hydrex.ion45@gmail.com>
2022-06-16 11:10:21 +0700

    first commit

---
+++ app/Controllers/Explore.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Explore extends BaseController
+{
+    public function index()
+    {
+        return view('user/explore');
+    }
+
+}
---
+++ app/Views/user/explore.php
@@ -0,0 +1,1 @@
+<center> <h1> Hai </h1> </center>
